4e4fb0b3023e086d35168cbe5126c6d903721a7d
UENO Yuuji <uenoyuuji@gmail.com>
2023-04-29 14:21:27 +0900

    viewへのパラメータの渡し方を変更

--- public/js/index.js
+++ public/js/index.js
@@ -1,41 +1,42 @@
-// range(0x0300, 0x036f).concat(range(0x20d0, 0x20ff));
-// range(0x0300, 0x036f).concat(range(0x20d0, 0x20e1));
-const COMBINING_CHARACTER_CODES = [
-    768,769,770,771,772,773,774,775,776,783,792,793,794,796,797,798,799,
-    800,804,805,807,808,809,810,812,815,816,818,820,825,826,827,828,829,832,833,836
-];
+"use strict";
+function main(locals) {
+    const COMBINING_CHARACTER_CODES = [
+        768, 769, 770, 771, 772, 773, 774, 775, 776, 783, 792, 793, 794, 796, 797, 798, 799,
+        800, 804, 805, 807, 808, 809, 810, 812, 815, 816, 818, 820, 825, 826, 827, 828, 829, 832, 833, 836
+    ];
 
-new Vue({
-    el: '#app',
-    data: Object.assign({
-        placeholder: 'Input here',
-        decorated_message: '',
-        name: '',
-        chats: []
-    }, LOCALS),
-    methods: {
-        decorate: function(str, min, max) {
-            let ret = '';
-            str.split('').forEach(function(chr) {
-                let codes = [chr.charCodeAt(0)];
-                for(let i = 0, l = choice(range(min, max)); i < l; i++) {
-                    codes[codes.length] = choice(COMBINING_CHARACTER_CODES);
-                }
-                ret += String.fromCharCode.apply(null, codes);
-            });
-            return ret;
+    new Vue({
+        el: '#app',
+        data: Object.assign({
+            placeholder: 'Input here',
+            decorated_message: '',
+            name: '',
+            chats: []
+        }, locals),
+        methods: {
+            decorate: function (str, min, max) {
+                let ret = '';
+                str.split('').forEach(function (chr) {
+                    let codes = [chr.charCodeAt(0)];
+                    for (let i = 0, l = choice(range(min, max)); i < l; i++) {
+                        codes[codes.length] = choice(COMBINING_CHARACTER_CODES);
+                    }
+                    ret += String.fromCharCode.apply(null, codes);
+                });
+                return ret;
+            }
         }
-    }
-});
+    });
 
-function range(begin, end) {
-    let a = [];
-    for(let i = begin; i <= end; i++) {
-        a[a.length] = i;
+    function range(begin, end) {
+        let a = [];
+        for (let i = begin; i <= end; i++) {
+            a[a.length] = i;
+        }
+        return a;
     }
-    return a;
-};
 
-function choice(arr) {
-    return arr[Math.floor(arr.length * Math.random())];
-};
+    function choice(arr) {
+        return arr[Math.floor(arr.length * Math.random())];
+    }
+}
